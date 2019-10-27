c1d1a694bce8b40b06249f36a07bcaf84cd1a858
Karen Garcia <kgarcia@i3screen.com>
2019-10-27 13:18:43 -0600

    Trying out solution to solve refresh issue going to 404 site

--- src/Components/App/Navigation.js
+++ src/Components/App/Navigation.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom';
 import MyWork from './MyWork';
 import ContactMe from './ContactMe';
 import AboutMe from './AboutMe';
 import Home from './HomeSection';
+import NotFound from './NotFound.js';
 
 const NavigationBar = () => (
      <Router>
@@ -27,15 +28,22 @@ const NavigationBar = () => (
           </div>
         </nav>
       </div>    
-        <Route exact strict path ="/" component={Home}/>
-        <Route path={"/Home"} component={Home}/>
-        <Route path={"/AboutMe"} component={AboutMe}/>
-        <Route path={"/MyWork"} component={MyWork}/>
-        <Route path={'/contact'} component={ContactMe} />
-        <Route path={'/saykaren.github.io/saykarenaugsix_deploy/'} component={Home} />
-        <Route exact strict path={"https://saykaren.github.io/"} component={Home} />
-        <Route exact strict path={"https://saykaren.github.io/Home"} component={Home} />
-        <Route exact strict path ="/saykaren.github.io/saykarenaugsix_deploy/" component={Home}/>
+        <Switch>
+          <Route path={"/Home"} component={Home}/>
+          <Route path={"/AboutMe"} component={AboutMe}/>
+          <Route path={"/MyWork"} component={MyWork}/>
+          <Route path={'/contact'} component={ContactMe} />
+          <Route exact strict path ="/" component={Home}/>
+          <Route path={'/saykaren.github.io/saykarenaugsix_deploy/'} component={Home} />
+          <Route exact strict path={"https://saykaren.github.io/"} component={Home} />
+          <Route exact strict path={"https://saykaren.github.io/Home"} component={Home} />
+          <Route exact strict path ="/saykaren.github.io/saykarenaugsix_deploy/" component={Home}/>
+          <Route exact strict path ="/saykaren.github.io/MyWork" component={MyWork}/>
+          <Route exact strict path ="/saykaren.github.io/portfolio" component={Home}/>
+          <Route exact strict path ="/saykaren.github.io/AboutMe" component={AboutMe}/>
+          <Route exact strict path ="/saykaren.github.io/AboutMe" component={AboutMe}/>
+          <Route path="*" component={NotFound} />
+        </Switch>
       </Router>
   );
 
---
+++ src/Components/App/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Home from './HomeSection';
+
+const NotFound = () => (
+   <Home />
+);
+
+export default NotFound;
